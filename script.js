cb2bc3004977b0607c715a0a0968b42c775b8a75
Roseanne Nguyen <rtnguye1@gmail.com>
2020-12-06 22:58:51 -0700

    worked on html and js

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 
-// click start button. Need to set timer 
-// then timer starts and questions is presented
-// need 4 options and ul/li
-// start with an array for questions
 
-var quizQuestions = [{ 
+var questionElement = document.getElementById("question");
+var choiceElement = document.getElementById("options");
+var resultsFinal = document.getElementById("results");
+// storage
+var storedScores = JSON.parse(localStorage.getItem("userData"))
+var allScores = [];
+
+// timer
+var timer = 60;
+        
+function setTimer() {  var interval = setInterval(function(){
+document.getElementById("timer").innerHTML=timer;
+timer--;
+
+if (timer === 0){
+    clearInterval(interval);
+    document.getElementById("timer").innerHTML='Done';
+    alert("You're out of time!");
+    return;
+}
+}, 1000);
+}
+
+var currentQuestion = 0;
+var mainDisplay = document.getElementById("article");
+var startQuiz = document.getElementById("startButton");
+var nextQuestion =document.getElementById("nextButton");
+var questionContainer = document.getElementById("question-container");
+
+startQuiz.addEventListener("click", beginQuiz);
+
+function beginQuiz() {
+    console.log("Started");
+    setTimer();
+    firstQuestion();
+    questionElement.setAttribute("style", "display: block");
+    mainDisplay.setAttribute("style", "display: none");   
+}
+
+
+var score = 0;
+var totalTimer = 60;
+var pentalty = 10;
+var userChoice = "";
+var saveUserscore = [];
+
+  var quizQuestions = [{ 
     question: "What is javascript?",
     answer: "A programming language",
     choices: [
@@ -27,66 +69,56 @@ var quizQuestions = [{
     ],
 }];
 
-var userChoice = "";
-var user = [];
-var saveUserscore = [];
-var score = 0;
 
-function displayQuestion(question) {
-    document.getElementById("#question").innerHTML = ""; 
-    document.getElementById("#choices").innerHTML = ""; 
-
-    var choices =questions[currentIndex].choices;
+function getQuestion(question) {
 
     for (var i =0; i < question.choices.length; i++) {
-    var correctChoice = document.getElementById("#button");
-    var choiceBody = document.createElement("div")
+    var options = document.getElementById("#button");
+    // var choice = document.createElement("div")
 
-    choice.textContent = question.choices[i];
+    answerElement.textContent = question.choices[i];
 
-    choice.addEventListener("click", function(){
+    options.addEventListener("click", function(){
     checkAnswer(question, function (event) {
     event.preventDefault();
     });
     // when click, pass text through there. Look at the event objects for clicks (activities 12); look through the console
     });
 
-    document.getElementById("choices").appendChild(choice); 
+    document.getElementById("option").appendChild(choice); 
     }   
 
 }
 
+function nextQuestion () {
+    showQuestion(currentQuestion)
+}
 
 function checkAnswer(question, userChoice) {
     if (userChoice === question.answer) {
         score++;
             alert("That's correct!");
     }
+    else if (userchoice !== answer) {
+        removeTime(timer);
+    }
+}
+function showQuestion(question) {
+    questionElement.innerHTML = question.question;
+
 }
 
 
-// when question answer, then presented with another question
 
+// Storage
+function highscorePage(a, b) {
 
+    var userData = {
+        name: a,
+        userScore: b
+    };
+    allScores.push(userData);
 
-// when answer question incorrectly, then time is substracted from clock
+    localStorage.setItem("userData", JSON.stringify(allScores));
 
-
-// when all questions are answers or timer reachers 0
-
-
-
-// function holdingtext() {
-
-
-
-
-
-// then game over
-
-
-
-
-
-// when game over, then can save intials and score
-// text box and local storage 
+}
